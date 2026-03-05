6279ec9e68dc0eea203963aeaa51d1bf1052fa9a
Peterbubu <wpispanda@163.com>
2026-03-05 14:25:24 +0800

    Switch to HashRouter to fix routing issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import IdentityGuidePage from './pages/IdentityGuidePage';
 import AuthorInfoPage from './pages/AuthorInfoPage';
